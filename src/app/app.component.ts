e20141afbcd46d17a9c43bb9f4c105eaa7a6a34c
Vishal Aggarwal <vishal.aggarwal01@nagarro.com>
2018-10-17 09:18:25 +0530

    App component updated (router-outlet added)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ArticleService } from './article.service';
+import { TagsService } from './tags/tags.service';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,13 @@ import { ArticleService } from './article.service';
 export class AppComponent {
   title = 'conduict';
 
-  constructor(private articleService: ArticleService) {
+  constructor(private articleService: ArticleService,private tagService: TagsService) {
 
   }
 
   onTagSelection(value) {
     console.log(value)
+    this.tagService.setTagName(value)
     this.articleService.getFeedByTag(value)
   }
 }
